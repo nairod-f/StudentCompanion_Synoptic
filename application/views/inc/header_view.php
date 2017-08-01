a42a9b99e6ec0a3fcac25887926d79af24fcd083
nairod-f <Dorian.fenech.22@gmail.com>
2017-08-01 17:49:38 +0100

    Added the core CI files, bootstrap and Javascript
    the implementation of the javascript and bootstrap, aling with other config files and other elements

---
+++ application/views/inc/header_view.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="initial-scale=1, maximum-scale=1, user-scalable=no, width=device-width">
+
+        <title>MyApp</title>
+
+        <link type="text/css" rel="stylesheet" href="<?=base_url('public/CSS/bootstrap.min.css')?>">
+        <link type="text/css" rel="stylesheet" href="<?=base_url('public/CSS/style.css')?>">
+
+
+        <script type="text/javascript" src="<?=base_url('public/js/jquery.js')?>"></script>
+        <script type="text/javascript" src="<?=base_url('public/js/bootstrap.js')?>"></script>
+
+    </head>
+    <body>
